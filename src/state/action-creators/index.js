34abcb237c4b84d9963729e64ce5034e5a669f2b
Kartar Singh <k4kartar4520@gmail.com>
2023-09-22 15:04:57 +0530

    creating reducer and action-creator

---
+++ src/state/action-creators/index.js
@@ -0,0 +1,21 @@
+// return (dispatch) => {...}: Inside the depositMoney function, it returns another function that takes dispatch as a parameter. This inner function is often used in Redux to dispatch actions asynchronously.
+// dispatch({ type: 'deposit', payload: amount }): Within the inner function, it uses the dispatch function to send an action to the Redux store.
+
+
+export const depositMoney = (amount) =>{
+    return (dispatch)=>{
+        dispatch({
+            type:'deposit',
+            payload: amount
+        })
+    }
+}
+
+export const withdrawMoney = (amount) =>{
+    return (dispatch)=>{
+        dispatch({
+            type:'withdraw',
+            payload: amount
+        })
+    }
+}
